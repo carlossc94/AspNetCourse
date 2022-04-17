86f6a5bfbbffdf5816e8a4d2b329440728d9329c
Carlos Soberanis Cupul <carlos.soberanisc@outlook.com>
2022-04-17 16:02:40 -0500

    Modelo y visualización Básica (Fin Etapa1)

--- Controllers/EscuelaController.cs
+++ Controllers/EscuelaController.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-
+using platzi_asp_net_core.Models;
 namespace platzi_asp_net_core.Controllers
 {
     public class EscuelaController : Controller
     {
         public IActionResult Index(){
-            return View();
+            var Escuela = new Escuela();
+            Escuela.Nombre= "Platzi";
+            Escuela.Id=Guid.NewGuid().ToString();
+            Escuela.AnioFundacion=2018;
+            return View(Escuela);
         }
     }
 }
---
+++ Models/Escuela.cs
@@ -0,0 +1,9 @@
+namespace platzi_asp_net_core.Models
+{
+    public class Escuela
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }  
+        public int AnioFundacion { get; set; }
+    }
+}
